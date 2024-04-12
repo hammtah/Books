18b2423ac5c45805ff85cbc2d8a28f0c03a6a474
hammtah <115116715+hammtah@users.noreply.github.com>
2024-04-12 09:12:43 +0000

    get books and render them

---
+++ script.js
@@ -0,0 +1,47 @@
+let data=[];
+const booksUrl = "./data.json";
+
+getBooks(booksUrl)
+.then(()=>{
+    renderBooks(data)
+});
+
+async function getBooks(url){
+    const res = await fetch(url).then(res=> res.json());
+    data = res;
+}
+
+function renderBooks(data){
+    data.forEach( book=> renderBook(book) );
+}
+
+function renderBook(bookData){
+    const book = createBook();
+    const {id,category, img, title, author}=bookData;
+
+    book.querySelector(".book-div").id=`book-${id}`;
+    const categoryDom = book.querySelector(".category");
+    const imgDom = book.querySelector(".book-img");
+    const titleDom = book.querySelector(".book-title");
+    const authorDom = book.querySelector(".author");
+
+    updateElement(categoryDom, category);
+    updateElement(imgDom, img);
+    updateElement(titleDom, title);
+    updateElement(authorDom, author);
+    const app=document.getElementById("app");
+    app.appendChild(book)
+
+}
+
+function createBook(){
+    const template = document.getElementById("book");
+    const book = template.content.cloneNode(true);
+    return book;
+}
+
+function updateElement(node,content){
+    if(node.src) node.src=content;
+    else node.textContent=content;
+}
+
